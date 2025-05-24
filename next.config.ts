d67ec03810321906d0c0459d78b322b64f244f46
yexiaoli <yxl2106@163.com>
2025-05-24 18:02:26 +0800

    chore: init project

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,21 @@
-import type { NextConfig } from 'next'
-// import path from 'path'
-
-const nextConfig: NextConfig = {
-    // sassOptions: {
-    //     includePaths: [path.join(__dirname, 'styles')],
-    //     // 全局注入变量（支持 Element Plus 和自定义变量）
-    //     prependData: `
-    //     @use "styles/variables" as *;
-    //     `
-    // },
-    /* config options here */
-    webpack: (config, { dev }) => {
-        if (dev) {
-            config.watchOptions = {
-                poll: 1000, // 检查文件变化的频率（毫秒）
-                aggregateTimeout: 300 // 重新构建前的延迟
-            }
-        }
-        return config
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [] // 允许加载的图片域名
+    },
+    experimental: {
+        serverActions: true
+    },
+    async rewrites() {
+        return process.env.NODE_ENV === 'development'
+            ? [
+                  {
+                      source: '/api/:path*',
+                      destination: process.env.NEXT_PUBLIC_APP_BASE_API + '/:path*'
+                  }
+              ]
+            : []
     }
 }
 
